cfe6ec53dae931bcbe80c61332419f9f54110110
cshanes <cshanes@umich.edu>
2017-04-07 19:32:33 -0400

    initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "hf_predict"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ src/main/scala/edu/gatech/cs6250/main/Main.scala
@@ -0,0 +1,10 @@
+package edu.gatech.cs6250.main
+
+/**
+  * Created by chanes on 4/7/17.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
