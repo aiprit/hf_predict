0951b416dad9ffdb02bf9bd42bef1caf706a3a89
cshanes <cshanes@umich.edu>
2017-04-07 20:25:05 -0400

    got Main working

--- build.sbt
+++ build.sbt
@@ -2,5 +2,17 @@ name := "hf_predict"
 
 version := "1.0"
 
-scalaVersion := "2.12.1"
-    
+scalaVersion := "2.11.7"
+
+resolvers ++= Seq(
+  Resolver.sonatypeRepo("releases"),
+  Resolver.sonatypeRepo("snapshots")
+)
+
+val sparkVersion = "1.6.2"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "com.databricks"   %% "spark-csv" % "1.5.0",
+  "org.apache.spark" %% "spark-sql" % sparkVersion
+)
--- src/main/scala/edu/gatech/cs6250/main/Main.scala
+++ src/main/scala/edu/gatech/cs6250/main/Main.scala
@@ -1,10 +1,29 @@
 package edu.gatech.cs6250.main
 
+import com.databricks.spark.csv.CsvContext
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by chanes on 4/7/17.
   */
 object Main {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]) {
 
+    val sc = createContext
+    val sqlContext = new SQLContext(sc)
+    
+    val notes_df = sqlContext.csvFile("data/NOTEEVENTS.csv")
+    notes_df.registerTempTable("note_events")
+    println(notes_df.show(10))
   }
+
+  def createContext(appName: String, masterUrl: String): SparkContext = {
+    val conf = new SparkConf().setAppName(appName).setMaster(masterUrl)
+    new SparkContext(conf)
+  }
+
+  def createContext(appName: String): SparkContext = createContext(appName, "local")
+
+  def createContext: SparkContext = createContext("cs6250 project", "local")
 }
