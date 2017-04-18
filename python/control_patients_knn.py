58e4e95d3b28cf160d4c64a3bc324d4104033a9d
cshanes <cshanes@umich.edu>
2017-04-18 19:04:27 -0400

    fixed control patients and feature construction

--- python/control_patients_knn.py
+++ python/control_patients_knn.py
@@ -33,20 +33,48 @@ print(test_df.shape)
 
 # get the 9 closest patients by age and sex
 X = fit_df[['gender', 'age']].as_matrix()
-knn_mdl = NearestNeighbors(9).fit(X)
+knn_mdl = NearestNeighbors(50).fit(X)
 
-control_patients = pd.DataFrame()
+patient_id_set = set()
+
+print(fit_df.head())
+
+control_patient_rows = []
 for case in list(case_patient_ids):
     knn_data = knn_df[knn_df['subject_id'] == case][['gender', 'age']].as_matrix()
     distances, indices = knn_mdl.kneighbors(knn_data)
     hdfx = case_df[case_df['subject_id'] == case]['hdfx'].values[0]
-    for idx in indices:
-        patient_row = fit_df.iloc[idx]
-        patient_row['hdfx'] = hdfx
-        control_patients = control_patients.append(patient_row)
 
+    control_count = 0
+    for idx in indices[0]:
 
-print('control patients df')
+        if control_count == 10:
+            break
+
+        patient_row = list(fit_df.iloc[idx])
+        subject_id = patient_row[0]
+        gender = patient_row[1]
+        dob = patient_row[2]
+        age = patient_row[3]
+
+        patient_row_dict = {
+            'subject_id': subject_id,
+            'gender': gender,
+            'dob': dob,
+            'age': age,
+            'hdfx': hdfx
+        }
+
+        if subject_id in patient_id_set:
+            print('Duplicate control patient')
+            continue
+
+        patient_id_set.add(subject_id)
+        control_count += 1
+
+        control_patient_rows.append(patient_row_dict)
+
+control_patients = pd.DataFrame(control_patient_rows)
+# print('control patients df')
 print(control_patients.head(10))
 control_patients.to_csv('control_patients.csv')
-print(control_patients.shape)
