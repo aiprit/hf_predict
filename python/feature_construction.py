4502cb91751859fd794b82dfdd03f7cae4b8e395
cshanes <cshanes@umich.edu>
2017-04-25 19:24:14 -0400

    updated with new 3hf data

--- python/feature_construction.py
+++ python/feature_construction.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import csv
 
-df = pd.read_csv('patient_event_data.csv')
+df = pd.read_csv('patient_event_data_3hf.csv')
 
 df.sort_values(['subject_id', 'admittime', 'seq_num'])
 print(df.head())
@@ -14,16 +14,18 @@ result = []
 for patient_id in patient_id_set:
     patient_data = df[df['subject_id'] == patient_id]
     events = [patient_data['hf'].values[0]]
+
     for event in list(patient_data['admittime']):
     # for event in list(patient_data['event_id']):
         events.append(event)
+
     if len(events) < 50:
         continue
     result.append(events)
 
 print(len(result))
 
-# with open("feature_construction_output_50_events.csv", "w") as f:
-with open("feature_construction_output_50_dates.csv", "w") as f:
+# with open("feature_construction_output_50_events_3hf.csv", "w") as f:
+with open("feature_construction_output_50_dates_3hf.csv", "w") as f:
     writer = csv.writer(f)
     writer.writerows(result)
