6d2b5869a3bed4cd5b20ce7ef2f4220ec698f3f7
cshanes <cshanes@umich.edu>
2017-04-25 16:11:23 -0400

    new feature output for 50 dates

--- python/feature_construction.py
+++ python/feature_construction.py
@@ -14,8 +14,8 @@ result = []
 for patient_id in patient_id_set:
     patient_data = df[df['subject_id'] == patient_id]
     events = [patient_data['hf'].values[0]]
-    # for event in list(patient_data['admittime']):
-    for event in list(patient_data['event_id']):
+    for event in list(patient_data['admittime']):
+    # for event in list(patient_data['event_id']):
         events.append(event)
     if len(events) < 50:
         continue
@@ -23,7 +23,7 @@ for patient_id in patient_id_set:
 
 print(len(result))
 
-with open("feature_construction_output_50_events.csv", "w") as f:
-# with open("feature_construction_output_dates.csv", "w") as f:
+# with open("feature_construction_output_50_events.csv", "w") as f:
+with open("feature_construction_output_50_dates.csv", "w") as f:
     writer = csv.writer(f)
     writer.writerows(result)
